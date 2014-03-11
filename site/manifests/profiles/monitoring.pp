cceddd131339749e62b93709a5913b92bb5928ca
Christophe Roux <gloppasglop@gmail.com>
2014-03-11 16:58:59 +0100

    Add graphite, gdash,...

---
+++ site/manifests/profiles/monitoring.pp
@@ -0,0 +1,26 @@
+class site::profile::monitoring {
+  include ::graphite
+  include gdash
+
+  include apache::mod::passenger
+  $myvhosts = hiera('apache::vhosts', {})
+  create_resources('apache::vhost', $myvhosts)
+
+  gdash::category { "Expertday": }
+
+  gdash::dashboard { "OS_Metrics":
+        description => "OS Metrics",
+        category    => "Expertday",
+        require     => Gdash::Category["Expertday"],
+  }
+
+  gdash::dashboard { "Web_Server_Metrics":
+        description => "Web Server Metrics",
+        category    => "Expertday",
+        require     => Gdash::Category["Expertday"],
+  }
+
+  Gdash::Graph <<||>>
+  Gdash::Field <<||>>
+
+}
