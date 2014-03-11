084752f91eada73e76368ec021195e00b13dde7d
Christophe Roux <gloppasglop@gmail.com>
2014-03-11 16:34:34 +0100

    Move site.pp to different directory

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,3 +2,8 @@ node 'puppet.example.com' {
   class { '::site::roles::base': } ->
   class { '::site::roles::puppet::master': }
 }
+
+
+node 'b9836319-968f-444c-8906-a5ad104bf90c.localdomain' {
+  class { '::site::roles::base': }
+}
