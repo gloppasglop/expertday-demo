4073b0f70cefe68e1e25687555fc8af444719691
Christophe Roux <gloppasglop@gmail.com>
2014-03-27 14:48:08 +0100

    Correct role webserver

--- site/manifests/roles/webserver.pp
+++ site/manifests/roles/webserver.pp
@@ -1,5 +1,4 @@
 class site::roles::webserver inherits site::roles::base {
   notify { "site::roles::webserver": }
   include site::profiles::webserver
-  include site::profile::monitoring::webserver
 }
