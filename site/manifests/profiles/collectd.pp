67bcc23ffd64d0949e8f1af410cdf29826015f42
Christophe Roux <gloppasglop@gmail.com>
2014-09-02 17:16:50 +0200

    Use standard Ubuntu repo for collectd for 14.04

--- site/manifests/profiles/collectd.pp
+++ site/manifests/profiles/collectd.pp
@@ -2,6 +2,5 @@ class site::profiles::collectd {
   notify { "site::profiles::collectd": }
   include ::collectd
   include apt
-  apt::ppa { 'ppa:kmscherer/collectd': } -> Class['::collectd']
   include collectd::plugin::write_graphite
 }
