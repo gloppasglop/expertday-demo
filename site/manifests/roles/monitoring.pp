626c41582adc323cf21566391726cb6ba3b4ab52
Christophe Roux <gloppasglop@gmail.com>
2014-09-03 16:02:47 +0200

    Initial support grafana

--- site/manifests/roles/monitoring.pp
+++ site/manifests/roles/monitoring.pp
@@ -1,4 +1,5 @@
 class site::roles::monitoring inherits site::roles::base {
   notify { "site::roles::monitoring": } 
-  include site::profiles::monitoring
+  include site::profiles::graphite
+  include site::profiles::grafana
 } 
